9713b9da27d6a50439c4693e99ee001ba8d7b9dd
paymanferdosali <p.ferdosali@hotmail.com>
2017-02-25 16:46:10 -0800

    Apply grayscale and normalization

--- Trafic SIgn.py
+++ Trafic SIgn.py
@@ -18,16 +18,16 @@ X_test, y_test = test['features'], test['labels']
 #Step 1: Dataset Summary & Exploration
 ### Replace each question mark with the appropriate value.
 ### Use python, pandas or numpy methods rather than hard coding the results
-# TODO: Number of training examples
+# TODO: Number of training examples(done)
 import pandas
 n_train = train['features'].shape[0]
-# TODO: Number of testing examples.
+# TODO: Number of testing examples.(done)
 n_test = test['features'].shape[0]
-# TODO: What's the shape of an traffic sign image?
+# TODO: What's the shape of an traffic sign image?(done)
 image_shape = []
 image_shape.append(train['sizes'][0][0])
 image_shape.append(train['sizes'][0][1])
-# TODO: How many unique classes/labels there are in the dataset.
+# TODO: How many unique classes/labels there are in the dataset.(done)
 n_classes = len(set(train['labels']))
 print("Number of training examples =", n_train)
 print("Number of testing examples =", n_test)
@@ -42,14 +42,38 @@ import matplotlib.pyplot as plt
 #%matplotlib inline
 import random
 rnd_img = []
-for i in range(6):
-    index = random.randint(0,n_train)
-    rnd_img.append(X_train[index])
-    plt.subplot(3,2,i+1)
-    plt.title(str((train['labels'][index])),style='italic',
-        bbox={'facecolor':'red', 'alpha':0.5, 'pad':1})
-    plt.imshow(rnd_img[i])
-plt.show()
+#TODO: Uncoment here for submission
+# for i in range(6):
+#     index = random.randint(0,n_train)
+#     rnd_img.append(X_train[index])
+#     plt.subplot(3,2,i+1)
+#     plt.title(str((train['labels'][index])),style='italic',
+#         bbox={'facecolor':'red', 'alpha':0.5, 'pad':1})
+#     plt.imshow(rnd_img[i])
+# plt.show()
+
+#Step 2: Design and Test a Model Architecture
+### Preprocess the data here. Preprocessing steps could include normalization, converting to grayscale, etc.
+### Feel free to use as many code cells as needed.
+import cv2
+X_train_nrm=[]
+for i in range(n_train):
+    #make grayscale image
+    X_train_nrm.append((cv2.cvtColor(X_train[i],cv2.COLOR_BGR2GRAY)))
+    #normilize data
+    cv2.normalize(X_train_nrm[i],X_train_nrm[i],0,10,cv2.NORM_MINMAX)
+
+
+#TODO: Uncomment if you need to see the normilization pictures
+# for i in range(6):
+#     index = random.randint(0,n_train)
+#     rnd_img.append(X_train_nrm[index])
+#     plt.subplot(3,2,i+1)
+#     plt.title(str((train['labels'][index])),style='italic',
+#         bbox={'facecolor':'red', 'alpha':0.5, 'pad':1})
+#     plt.imshow(rnd_img[i],cmap='gray')
+# plt.show()
+
 
 
 
